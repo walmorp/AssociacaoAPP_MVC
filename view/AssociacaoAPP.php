3e99f69cb86ae113eadb35882ce651cb8827eabf
walmorp <walmorp@gmail.com>
2018-10-12 15:49:03 -0300

    Atualização para rotinas de testes phpunit

--- view/AssociacaoAPP.php
+++ view/AssociacaoAPP.php
@@ -24,25 +24,25 @@
                  <ul>
                   <li class='tituloMenu'>Menu</li>
                   <hr size='12'>
-                  <li class='menu'><a href="?class=Home"                     target="iframeCentro" title="Mostra página inicial">Início</a> </li>
+                  <li class='menu'><a id="Home"             href="?class=Home"                     target="iframeCentro" title="Mostra página inicial">Início</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleAssociado&opSA=A" target="iframeCentro" title="Inicia controle de associados">Controle de associados</a> </li>
+                  <li class='menu'><a id="Associado"        href="?class=ControleAssociado&opSA=A" target="iframeCentro" title="Inicia controle de associados">Controle de associados</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleTitulo&opST=A"    target="iframeCentro" title="Inicia controle de títulos dos associados">Controle de títulos</a> </li>
+                  <li class='menu'><a id="Titulo"           href="?class=ControleTitulo&opST=A"    target="iframeCentro" title="Inicia controle de títulos dos associados">Controle de títulos</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleCobranca"         target="iframeCentro" title="Inicia controle de parcelas">Controle de parcelas</a> </li>
+                  <li class='menu'><a id="Cobranca"         href="?class=ControleCobranca"         target="iframeCentro" title="Inicia controle de parcelas">Controle de parcelas</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleCidade"           target="iframeCentro" title="Inicia controle de cidades">Controle de cidades</a> </li>
+                  <li class='menu'><a id="Cidade"           href="?class=ControleCidade"           target="iframeCentro" title="Inicia controle de cidades">Controle de cidades</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleTipoCobranca"     target="iframeCentro" title="Inicia tipos de cobrança">Tipos de cobrança</a> </li>
+                  <li class='menu'><a id="TipoCobranca"     href="?class=ControleTipoCobranca"     target="iframeCentro" title="Inicia tipos de cobrança">Tipos de cobrança</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleAssociado&opSA=I" target="iframeCentro" title="Inicia associados inativos">Associados inativos</a> </li>
+                  <li class='menu'><a id="AssociadoInativo" href="?class=ControleAssociado&opSA=I" target="iframeCentro" title="Inicia associados inativos">Associados inativos</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=ControleTitulo&opST=I"    target="iframeCentro" title="Inicia associados inativos">Títulos inativos</a> </li>
+                  <li class='menu'><a id="TituloInativo"    href="?class=ControleTitulo&opST=I"    target="iframeCentro" title="Inicia associados inativos">Títulos inativos</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=RelatorioGerencial"       target="iframeCentro" title="Inicia relatório gerencial">Relatório gerencial</a> </li>
+                  <li class='menu'><a id="Gerencial"        href="?class=RelatorioGerencial"       target="iframeCentro" title="Inicia relatório gerencial">Relatório gerencial</a> </li>
                   <hr size='3'>
-                  <li class='menu'><a href="?class=RelatorioCobrador"        target="iframeCentro" title="Inicia relatório para cobrador">Relatório para cobrador</a> </li>
+                  <li class='menu'><a id="Cobrador"         href="?class=RelatorioCobrador"        target="iframeCentro" title="Inicia relatório para cobrador">Relatório para cobrador</a> </li>
                   <hr size='3'>
                  </ul>
               </td>
